d34663965d074a817808d6949b8af1e403530de7
cr4sh-me <cr4sh.lab@gmail.com>
2024-05-27 11:32:09 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RUST_PATH="/opt/cybertools/rust"
+RUST_PATH="/opt/cybertools/rust/lang"
 TOOLCHAIN="$(uname -m)-unknown-linux-gnu"
 TOOLCHAIN_VERSION="1.78.0"
 
@@ -15,3 +15,5 @@ RUSTUP_INIT_SKIP_PATH_CHECK=yes \
 CARGO_HOME=$RUST_PATH \
 RUSTUP_HOME=$RUST_PATH \
 sh rustup-init.sh -y --default-host "$TOOLCHAIN" --default-toolchain=$TOOLCHAIN_VERSION || exit 1
+
+chown -R "$SUDO_USER:sudo" $RUST_PATH
