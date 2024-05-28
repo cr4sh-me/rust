50d288afe1a376d4937feb035ddc927819d9b748
cr4sh-me <cr4sh.lab@gmail.com>
2024-05-28 14:11:00 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-RUST_PATH="/opt/cybertools/rust/lang"
 TOOLCHAIN="$(uname -m)-unknown-linux-gnu"
 TOOLCHAIN_VERSION="1.78.0"
 
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Run this as root idiot!"
-    exit 1
-fi
-
-rm -rf $RUST_PATH
-
-RUSTUP_INIT_SKIP_PATH_CHECK=yes \
-CARGO_HOME=$RUST_PATH \
-RUSTUP_HOME=$RUST_PATH \
+rm -rf "$HOME/.cargo" "$HOME/.rustup"
 sh rustup-init.sh -y --default-host "$TOOLCHAIN" --default-toolchain=$TOOLCHAIN_VERSION || exit 1
-
-chown -R "$SUDO_USER:sudo" $RUST_PATH
